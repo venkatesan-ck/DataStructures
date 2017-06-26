0b43f89e7117382f66b749ca0b557903f49a68af
Venkatesan C K <c.k.venkatesan@gmail.com>
2017-06-26 21:02:43 +0530

    Add Anagrams algo

---
+++ src/com/venky/strings/Anagrams.java
@@ -0,0 +1,45 @@
+package com.venky.strings;
+
+/**
+ * The Class Anagrams.
+ */
+public class Anagrams {
+
+	/**
+	 * Prints the anagrams.
+	 *
+	 * @param str
+	 *            the str
+	 */
+	public static void printAnagrams(String str) {
+		recPrintAnagrams("", str);
+	}
+
+	/*
+	 * Recursively print anagrams.
+	 *
+	 * @param prefix the prefix
+	 * 
+	 * @param str the str
+	 */
+	private static void recPrintAnagrams(String prefix, String str) {
+		if (str.isEmpty()) { // String is empty
+			System.out.println(prefix);
+		} else {
+			int n = str.length();
+			for (int i = 0; i < n; i++) { // Cut and add characters to prefix
+				recPrintAnagrams(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
+			}
+		}
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 */
+	public static void main(String[] args) {
+		printAnagrams("Cat");
+	}
+}
